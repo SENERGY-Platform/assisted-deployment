ca84e7bbaba5af6310b93c871116bf18344d8cfa
y-du <42994541+y-du@users.noreply.github.com>
2020-01-30 21:53:35 +0100

    call kubectl as subprocess, refactor

--- assistant/deployment.py
+++ assistant/deployment.py
@@ -3,25 +3,28 @@ from .logger import getLogger
 from .browser import Browser
 from .workload import WorkloadConfigs
 from .util import populateWorkload
-import os
+import os, subprocess
 
 
 logger = getLogger(__name__.rsplit(".", 1)[-1])
 
 
 class KubectlManager:
+    __tmp_workload_file = "{}/tmp_workload.yaml".format(user_dir)
+
     def __init__(self, browser: Browser, workload_configs: WorkloadConfigs):
         self.__browser = browser
         self.__workload_configs = workload_configs
 
     def deployWorkload(self, project, namespace, workload):
+        workload_file = "{}/{}/{}/{}".format(config.Browser.path, project, namespace, workload)
         configs = self.__workload_configs.getConfig(project, namespace, workload)
         if configs:
-            populateWorkload("{}/{}/{}/{}".format(config.Browser.path, project, namespace, workload), configs, "{}/tmp_wl.yaml".format(user_dir))
-            print("kubectl apply -f {}/tmp_wl.yaml --namespace={}".format(user_dir, namespace))
-            os.remove("{}/tmp_wl.yaml".format(user_dir))
-        else:
-            print("kubectl apply -f {}/{}/{}/{} --namespace={}".format(config.Browser.path, project, namespace, workload, namespace))
+            populateWorkload(workload_file, configs, self.__tmp_workload_file)
+            workload_file = self.__tmp_workload_file
+        subprocess.call(("kubectl", "apply", "-f", workload_file, "-n", namespace), stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL) # todo -> returncode
+        if os.path.isfile(self.__tmp_workload_file):
+            os.remove(self.__tmp_workload_file)
 
     def deployNamespace(self, project, namespace):
         for workload in self.__browser.listWorkloads(project, namespace):
