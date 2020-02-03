4a8df3227446250a3bf5bc6729a10f112382b38b
y-du <42994541+y-du@users.noreply.github.com>
2020-02-03 15:55:24 +0100

    rename methods and remove redundancies

--- assistant/deployment.py
+++ assistant/deployment.py
@@ -33,35 +33,46 @@ class KubectlManager:
         self.__browser = browser
         self.__workload_configs = workload_configs
 
-    def deployWorkload(self, project, namespace, workload):
+    def operationWorkload(self, cmd, project, namespace, workload):
         workload_file = "{}/{}/{}/{}".format(config.Browser.path, project, namespace, workload)
         configs = self.__workload_configs.getConfig(project, namespace, workload)
         if configs:
             populateWorkload(workload_file, configs, self.__tmp_workload_file)
             workload_file = self.__tmp_workload_file
-        cpi = subprocess.run(("kubectl", "apply", "-f", workload_file, "-n", namespace), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        cpi = subprocess.run(
+            ("kubectl", cmd, "-f", workload_file, "-n", namespace),
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT
+        )
+        log_msg = "kubectl {c} -f {p}/{n}/{w} -n {n}: {cpi}".format(
+            c=cmd,
+            p=project,
+            n=namespace,
+            w=workload,
+            cpi=cpi.stdout.decode().rstrip("\n").replace("\n", " - ")
+        )
         if cpi.returncode:
-            logger.error("kubectl apply -f {p}/{n}/{w} -n {n}: {cpi}".format(p=project, n=namespace, w=workload, cpi=cpi.stdout.decode().rstrip("\n").replace("\n", " - ")))
+            logger.error(log_msg)
         else:
-            logger.info("kubectl apply -f {p}/{n}/{w} -n {n}: {cpi}".format(p=project, n=namespace, w=workload, cpi=cpi.stdout.decode().rstrip("\n").replace("\n", " - ")))
+            logger.info(log_msg)
         if os.path.isfile(self.__tmp_workload_file):
             os.remove(self.__tmp_workload_file)
         return "{}/{}/{}".format(project, namespace, workload), cpi.returncode
 
-    def deployNamespace(self, project, namespace):
+    def operationNamespaceWorkloads(self, cmd, project, namespace):
         results = list()
         for workload in self.__browser.listWorkloads(project, namespace):
-            results.append(self.deployWorkload(project, namespace, workload))
+            results.append(self.operationWorkload(cmd, project, namespace, workload))
         return results
 
-    def deployProject(self, project):
+    def operationProjectWorkloads(self, cmd, project):
         results = list()
         for namespace in self.__browser.listNamespaces(project):
-            results.extend(self.deployNamespace(project, namespace))
+            results.extend(self.operationNamespaceWorkloads(cmd, project, namespace))
         return results
 
-    def deployAll(self):
+    def operationAllWorkloads(self, cmd):
         results = list()
         for project in self.__browser.listProjects():
-            results.extend(self.deployProject(project))
+            results.extend(self.operationProjectWorkloads(cmd, project))
         return results
